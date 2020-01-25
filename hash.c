47c473441726c280d72a519f078685ed1728df85
asaf129@gmail.com <asaf129@gmail.com>
2020-01-25 11:51:30 +0200

    list of ints is working, now work on array of lists (hash table)

--- hash.c
+++ hash.c
@@ -1,12 +1,45 @@
 #include "header.h"
-#define BUFSIZE 100
 
+
+
+void push(node_int * head, int val)
+{
+    node_int * current = head;
+    
+
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+        
+    /* now we can add a new variable */
+    current->next = (node_int *) malloc(sizeof(node_int));
+    current->next->val = val;
+    current->next->next = NULL;
+
+    
+    
+}
+
+void print_list(node_int * head)
+{
+    node_int * current = head;
+
+    while (current != NULL)
+    {
+        printf("%d\n", current->val);
+        current = current->next;
+    }
+}
 
 int main(int argc, char **argv)
 {
-    int i = 1;    
-    FILE *fp;
-    char buff[BUFSIZE]; /* a buffer to hold what you read in */
+    int i = 1; /* argv[0] is the program itself, so we need to iterate arguments from 1 */    
+    FILE *fp; 
+    char buff[BUFSIZE]; /* a buffer to hold the data from the input */
+    node_int * head = (node_int *) malloc(sizeof(node_int));
+    node_int hash_table[29];
+
 
     /* handle input */
     while(i < argc)
@@ -20,19 +53,17 @@ int main(int argc, char **argv)
         {
             printf("%d\n", atoi(buff));
             
+            /*push(hash_table[atoi(buff)],1);*/
         }
             
-
-
-
-
-        
-
         fclose(fp);  /* close the file */ 
 
         i++;
     }
-        
+    
+    printf("\n PRINT LIST \n");
+    push(hash_table[1],1);
+    print_list(hash_table[1]);        
  
     printf("\n");
     return 0;
--- header.h
+++ header.h
@@ -4,6 +4,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#define NUMBER_OF_STRINGS 29
-#define MAX_STRING_SIZE 20
+#define BUFSIZE 100
 
+/* define a list of ints */
+typedef struct node {
+    int val;
+    struct node * next;
+}node_int;
+
+typedef node_int hash_table[29];
+
+/* actions on the list */
+void push(node_int * head, int val);
+void print_list(node_int * head);
